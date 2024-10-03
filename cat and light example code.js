c437f470fbacaec9d65114a6e16bf57ddcc2d7c3
Satheesh <128798102+VinukondaSatheesh@users.noreply.github.com>
2024-10-03 14:18:21 +0530

    Add files via upload

---
+++ cat and light example code.js
@@ -0,0 +1,16 @@
+function switchoff() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-off-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-eyes-img.png";
+    document.getElementById("switchStatus").textContent = "Switched Off";
+    document.getElementById("onswitch").style.backgroundColor = "#22c55e";
+    document.getElementById("offswitch").style.backgroundColor = "#cbd2d9";
+}
+
+
+function switchon() {
+    document.getElementById("bulbImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/bulb-go-on-img.png";
+    document.getElementById("catImage").src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/cat-img.png";
+    document.getElementById("switchStatus").textContent = "Switched On";
+    document.getElementById("onswitch").style.backgroundColor = "#cbd2d9";
+    document.getElementById("offswitch").style.backgroundColor = "#e12d39";
+}
